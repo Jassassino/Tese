75c7f5739c89d0ceb1d4950d598817c296721235
Jassassino <52531888+Jassassino@users.noreply.github.com>
2022-09-10 10:28:47 +0100

    Motor and Steering Control added
    Motor works with ESC, they calibrate themselves also

--- LoRa_comms_working_v7_w_steer_esc/rf95_Autonomous_Boat/rf95_Autonomous_Boat.ino
+++ LoRa_comms_working_v7_w_steer_esc/rf95_Autonomous_Boat/rf95_Autonomous_Boat.ino
@@ -2,24 +2,39 @@
 
 #include <SPI.h>
 #include <RH_RF95.h>
+#include <ServoTimer2.h>
+
+ServoTimer2 ESC;     // create servo object to control the ESC
+ServoTimer2 LEME;    //Servo object to control the steering
 
 // Singleton instance of the radio driver
 RH_RF95 lora;
 
-char boatname[] = "$ASV01$";
-//////////////////////////
+//////////////////////////////////////////////////////////////////////
 char readserial[RH_RF95_MAX_MESSAGE_LEN]={0};//="Idling";
 uint8_t i = 0;
+int val; //Steering Angle
 
-
-void setup() 
-{
-
+void setup() { 
+  //Motor and Steering control
+  
+  // Attach the ESC on pin 9
+  ESC.attach(9); // (pin, min pulse width, max pulse width in microseconds) 
+  ESC.write(8);
+  delay(2000);
+  ESC.write(0);
+  
+  //Attach LEME to pin 10
+  LEME.attach(10); // (pin, min pulse width, max pulse width in microseconds) 
+  LEME.write(5);
+  
+  //USB to Raspi
   Serial.begin(9600);
   while (!Serial) ; // Wait for serial port to be available
+  
+  //LoRa Initialization
   if (!lora.init())
     Serial.println("init failed");
-
   lora.setFrequency(868.1);
   lora.setModemConfig(RH_RF95::Bw125Cr45Sf128);
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
@@ -47,17 +62,33 @@ void serialEvent(){
       readserial[i]=Serial.read();
       i++;
   }
+
+  /*
+  if(readserial[1]=='D'){
+    Steering(readserial);
+    if(readserial[6]=='S'){
+      speedcontrol(readserial)
+    }
+  }
+  if (readserial[1]=='S'){
+    speedcontrol(readserial);
+    if(readserial[6]=='D'){
+      Steering(readserial)
+    }
+  }
+  
+  */
   SendMessageLoRa(readserial, i);
   i=0;
   //Serial.println(readserial);
-  SendMessageLoRa(boatname,7);
 }
+
+
 
 void SendMessageLoRa(uint8_t *data, int datasize){
   lora.send(data, datasize);
   lora.waitPacketSent();
 
-  
 }
 
 void RecvMessageLoRa(){
@@ -75,7 +106,6 @@ void RecvMessageLoRa(){
         //Serial.print("Data Received:");
         Serial.println((char*)buf);
       }
- 
     }
     else
     {
@@ -86,4 +116,14 @@ void RecvMessageLoRa(){
   {
     Serial.println("No LoRa");
   }
+}
+
+void write2esc(int Value){
+ Value = map(Value, 0, 10, 1000, 1800);   // scale it to use it with the servo library (value between 0 and 180)
+ ESC.write(Value);    // Send the signal to the ESC
+}
+
+void write2leme(int Value){
+ Value = map(Value, 0, 10, 1000, 1800);   // scale it to use it with the servo library (value between 0 and 180)
+ LEME.write(Value);    // Send the signal to the ESC
 }
